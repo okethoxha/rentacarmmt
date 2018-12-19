cfd985cc0268301cc0c4b2e33693f5c575eacd05
unknown <oket.hoxha@gmail.com>
2018-12-19 21:18:28 +0100

    Refactoring with the exception entities added for handling exceptions

--- rentacar/src/main/java/io/appwebgo/rentacar/services/ProjectService.java
+++ rentacar/src/main/java/io/appwebgo/rentacar/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.appwebgo.rentacar.services;
 
 import io.appwebgo.rentacar.Domain.Project;
+import io.appwebgo.rentacar.exceptions.ProjectIdException;
 import io.appwebgo.rentacar.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,13 @@ public class ProjectService {
 
     public Project saveOrUpdateProject(Project project){
 
-        //Logic
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch(Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
 
-        return projectRepository.save(project);
     }
 
 }
--- rentacar/src/main/java/io/appwebgo/rentacar/web/ProjectController.java
+++ rentacar/src/main/java/io/appwebgo/rentacar/web/ProjectController.java
@@ -1,17 +1,22 @@
 package io.appwebgo.rentacar.web;
 
 import io.appwebgo.rentacar.Domain.Project;
+import io.appwebgo.rentacar.services.MapValidationErrorService;
 import io.appwebgo.rentacar.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -20,14 +25,19 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
 
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-        if(result.hasErrors())
-        {
-            return new ResponseEntity<String>("Invalid project object!", HttpStatus.BAD_REQUEST);
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+
+        if(errorMap != null){
+            return errorMap;
         }
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
